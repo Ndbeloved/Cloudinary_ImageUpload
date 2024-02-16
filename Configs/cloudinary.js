111d4b6f4eb9c7d32c228f0b011940df38ba8512
Ndbeloved <ndbeloved001@gmail.com>
2024-02-16 05:55:52 -0800

    Setting up for multiple image uploads

--- Configs/cloudinary.js
+++ Configs/cloudinary.js
@@ -1,19 +1,38 @@
 const cloudinary = require('cloudinary').v2
+const multer = require('multer')
+const {Readable} = require('stream')
 
-//configuring with api credentials
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
+module.exports = async function UploadImage(imagePaths){
+    //configuring with api credentials
+    cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUD_API_KEY,
+        api_secret: process.env.CLOUD_API_SECRET
+    })
 
-module.exports = async function UploadImage(imagePath){
+    
+
+    //converting to readable streams
+    const bufferToStream = (buffer)=>{
+        const stream = new Readable()
+        stream.push(buffer)
+        stream.push(null)   //this indicates to the consumer of the stream that theres no more data to read 
+        return stream
+    }
+
+
     try{
-        const result = await cloudinary.uploader.upload(imagePath)
-        return result.secure_url
+        const uploadPromise = imagePaths.map(async(image)=>{
+            // const stream = bufferToStream(image.buffer)
+            const stream = image.buffer
+            const result = await cloudinary.uploader.upload(stream, {resource_type: 'auto'})
+            return result.secure_url
+        })
+
+        const urls = await Promise.all(uploadPromise)
+        return urls
     }
     catch(err){
         console.log('Error uploading image: ', err)
-        res.status(500).json({"message": "Error uploading image"})
     }
 }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const multer = require('multer')
 const express = require('express')
 const app = express()
 const PORT = 3000
@@ -6,12 +7,21 @@ const PORT = 3000
 
 const ImageUploader = require('./Configs/cloudinary')
 
-
-
 app.use(express.json())
 
-app.post('/upload', async(req, res)=>{
 
+//setting multer for image upload
+const upload = multer({storage: multer.memoryStorage()})
+
+app.post('/upload', upload.array('images'),async(req, res)=>{
+    try{
+        const urls = ImageUploader(req.files)
+        res.status(200).json({urls})
+    }
+    catch(err){
+        console.log('error trying to upload: ', err)
+        res.status(500).json({"message": "error trying to upload"})
+    }
 })
 
 app.listen(PORT, ()=>{
